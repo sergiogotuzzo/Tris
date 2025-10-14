4dda7a6ba44ff1abd6094272e74d48fd8426407f
XenKys <sergiotuzzo@gmail.com>
2025-10-14 15:14:11 +0200

    fix: tie

--- main.c
+++ main.c
@@ -70,6 +70,9 @@ int main() {
             ) {
                 play = false;
                 printf("%c won!\n", lastPlayer);
+            } else if (c1 != '-' && c2 != '-' && c3 != '-' && c4 != '-' && c5 != '-' && c6 != '-' && c7 != '-' && c8 != '-' && c9 != '-') {
+                play = false;
+                printf("Tie!");
             }
     }
 
